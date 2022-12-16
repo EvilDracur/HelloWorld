825ddbfd7dd952c02dc0ec9e9006ecfbbc58d662
Bayrakovskiy Stanislav <evildracur@gmail.com>
2022-12-16 14:16:30 +0300

    Change output text.

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,10 +1,9 @@
-﻿Console.Write("=== ");
+﻿Console.Write("*** ");
 Console.Write("Hello, GeekBrains!");
-Console.Write(" ===");
+Console.Write(" ***");
 
 Console.WriteLine(); // Далее, мы переходим на следующую строку
                      // без дополнительного вывода информации
-
 
 Console.WriteLine("===========Bye!==========="); // Выводим строку на экран консоли,
                                                  // переходим на следующую строку
